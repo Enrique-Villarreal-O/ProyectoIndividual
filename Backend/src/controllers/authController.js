c68c7388a679405c6cd99aca2085aec235e471be
Enrique Villarreal <kikesis@gmail.com>
2025-01-09 10:10:04 -0500

    Update authController.js

---
+++ Backend/src/controllers/authController.js
@@ -0,0 +1,29 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const User = require('../models/User');
+
+exports.register = async (req, res) => {
+  try {
+    const user = await User.create(req.body);
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET);
+    res.status(201).json({ user, token });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ where: { email } });
+
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+      throw new Error('Invalid credentials');
+    }
+
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET);
+    res.json({ user, token });
+  } catch (error) {
+    res.status(401).json({ error: error.message });
+  }
+};
